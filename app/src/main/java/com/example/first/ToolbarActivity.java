88ad887f6f9a4d3aede103ce46885ec8e4cd0404
Administrator <2191018250@qq.com>
2018-09-14 10:27:09 +0800

    app

--- app/src/main/java/com/example/first/ToolbarActivity.java
+++ app/src/main/java/com/example/first/ToolbarActivity.java
@@ -1,8 +1,10 @@
 package com.example.first;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
 public class ToolbarActivity extends AppCompatActivity {
@@ -16,5 +18,19 @@ public class ToolbarActivity extends AppCompatActivity {
         //给toolbar加载菜单
         //setSupportActionBar(toolbar);//需要重写onCreateOptionMenu()和onOptionItemsSelected()
         toolbar.inflateMenu(R.menu.demo);//需要设置菜单选择的事件监听器
+        toolbar.setTitle("详情页");
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        toolbar.setLogo(R.mipmap.ic_notifications);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.menu_search:
+                        Snackbar.make(toolbar,"toolbar search",Snackbar.LENGTH_LONG).show();
+                                break;
+                }
+                return true;
+            }
+        });
     }
 }
